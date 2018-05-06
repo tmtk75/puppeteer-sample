473194d6ef7433d084408d520c7f9cf5ceeb47d1
Tomotaka Sakuma <ktmtmks@gmail.com>
2018-05-06 13:48:17 +0900

    Take full page

--- src/example.ts
+++ src/example.ts
@@ -5,8 +5,8 @@ const url = process.argv[2] || 'https://example.com';
 (async () => {
   const browser = await launch();
   const page = await browser.newPage();
-  await page.goto(url);
-  await page.screenshot({path: 'example.png'});
+  await page.goto(url, {waitUntil: ["networkidle2"]});
+  await page.screenshot({path: 'example.png', fullPage: true});
 
   await browser.close();
 })();
