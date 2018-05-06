f086177d12246180351085d4e09e2a5d0f79456d
Tomotaka Sakuma <ktmtmks@gmail.com>
2018-05-06 10:19:33 +0900

    Add sample codes

--- src/example.ts
+++ src/example.ts
@@ -1,9 +1,11 @@
-import puppeteer from 'puppeteer';
+import * as puppeteer from 'puppeteer';
+
+const url = process.argv[2] || 'https://example.com';
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto(url);
   await page.screenshot({path: 'example.png'});
 
   await browser.close();
